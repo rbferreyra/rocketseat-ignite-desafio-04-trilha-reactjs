b9b3a62375e36d4e0c7af849a8dc29ed745af4c3
Ronaldo Ferreyra <rferreyra@reweb.com.br>
2021-03-25 22:45:38 -0300

    Criar componente Food

---
+++ src/components/Food/index.tsx
@@ -0,0 +1,71 @@
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+
+import { Container } from './styles';
+
+interface FoodInterface {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    available: boolean;
+    image: string;
+}
+
+interface FoodProps {
+    food: FoodInterface
+}
+
+const Food = ({ food }: FoodProps): JSX.Element => {
+    return (
+        <Container>
+            <header>
+                <img src={food.image} alt={food.name} />
+            </header>
+            <section className="body">
+                <h2>{food.name}</h2>
+                <p>{food.description}</p>
+                <p className="price">
+                    R$ <b>{food.price}</b>
+                </p>
+            </section>
+            <section className="footer">
+                <div className="icon-container">
+                    <button
+                        type="button"
+                        className="icon"
+                        // onClick={this.setEditingFood}
+                        data-testid={`edit-food-${food.id}`}
+                    >
+                        <FiEdit3 size={20} />
+                    </button>
+
+                    <button
+                        type="button"
+                        className="icon"
+                        // onClick={() => handleDelete(food.id)}
+                        data-testid={`remove-food-${food.id}`}
+                    >
+                        <FiTrash size={20} />
+                    </button>
+                </div>
+
+                <div className="availability-container">
+                    <p>Disponível</p>
+
+                    <label htmlFor={`available-switch-${food.id}`} className="switch">
+                        <input
+                            id={`available-switch-${food.id}`}
+                            type="checkbox"
+                            checked
+                            // onChange={this.toggleAvailable}
+                            data-testid={`change-status-food-${food.id}`}
+                        />
+                        <span className="slider" />
+                    </label>
+                </div>
+            </section>
+        </Container>
+    );
+};
+
+export default Food;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Food from "../../components/Food";
 import Header from "../../components/Header"
+import api from "../../services/api";
+import { FoodsContainer } from "./styles";
+
+interface FoodInterface {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+}
 
 const Dashboard = (): JSX.Element => {
+  const [foods, setFoods] = useState<FoodInterface[]>([]);
+
+  useEffect(() => {
+    async function loadFoods() {
+      await api.get('foods').then(response => {
+        setFoods(response.data);
+      });
+    }
+
+    loadFoods();
+  }, []);
+
   return (
     <>
       <Header />
+      <FoodsContainer data-testid="foods-list">
+      {foods &&
+            foods.map(food => (
+              <Food
+                key={food.id}
+                food={food}
+              />
+            ))}
+      </FoodsContainer>
     </>
   )
 };
